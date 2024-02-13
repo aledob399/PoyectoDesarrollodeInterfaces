56e86611bd1bd790c54efcdfff6869a1fd8ee739
aledob399 <aledob399@alu.centroaduera.es>
2024-02-13 16:32:52 +0100

    a

--- app/src/main/java/com/example/poyectodesarrollodeinterfaces/PeliculasSeries.kt
+++ app/src/main/java/com/example/poyectodesarrollodeinterfaces/PeliculasSeries.kt
@@ -27,17 +27,14 @@ class PeliculasSeries : AppCompatActivity() {
         for (p in pelis) {
             pelisCollection.add(p)
                 .addOnSuccessListener {
-                    // Película agregada exitosamente
+                    Toast.makeText(this, "Pelicula añadida correctamente", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener { e ->
                     // Error al agregar la película
                     Toast.makeText(this, "Error al añadir la pelicula", Toast.LENGTH_SHORT).show()
                 }
-            val user = User(
-                "prueba", "hola@gmail.com"," holaaaa")
-            firestore.collection("usuarios").add(user)
-            val peli=Peli("Spiderman 2")
-            firestore.collection("peli").add(peli)
+
+
         }
         // Agregar las películas a la base de datos Firestore
        // agregarPelicula(pelis[0])
@@ -47,7 +44,7 @@ class PeliculasSeries : AppCompatActivity() {
         for (s in series) {
             seriesCollection.add(s)
                 .addOnSuccessListener {
-                    // Serie agregada exitosamente
+                    Toast.makeText(this, "Serie añadida correctamente", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener { e ->
                     // Error al agregar la serie
@@ -120,15 +117,15 @@ class PeliculasSeries : AppCompatActivity() {
 
         // Agregar películas a la lista
         peliculas.add(Pelicula("Max payne", "Para resolver una serie de asesinatos en Nueva York se unen un detective de policía y un asesino, que serán perseguidos por la policía, la mafia y una corporación despiadada.", R.drawable.maxpayne, "https://www.youtube.com/watch?v=GklHaGfncJI"))
-        peliculas.add(Pelicula("El hombre lobo", "Al regresar a su tierra ancestral, un hombre americano es mordido y posteriormente maldecido por un hombre lobo.", R.drawable.elhombrelobo, ""))
-        peliculas.add(Pelicula("John Wick (Otro día para matar)", "Un ex-sicario sale de su retiro para perseguir a los gángsters que mataron a su perro y le robaron el coche.", R.drawable.johnwick, ""))
-        peliculas.add(Pelicula("Juego de armas", "Basada libremente en la historia real de dos jóvenes, David Packouz y Efraim Diveroli, que consiguieron un contrato de trescientos millones de dólares del Pentágono para armar a los aliados de Estados Unidos en Afganistán.", R.drawable.juegodearmas, ""))
-        peliculas.add(Pelicula("Los odiosos ocho", "En pleno invierno de Wyoming, un cazarrecompensas y su prisionero encuentran refugio en una cabaña habitada actualmente por una colección de nefastos personajes.", R.drawable.losodiososocho, ""))
-        peliculas.add(Pelicula("Seven", "Dos detectives, un novato y un veterano, dan caza a un asesino en serie que utiliza los siete pecados capitales como motivos.", R.drawable.seven, ""))
+        peliculas.add(Pelicula("El hombre lobo", "Al regresar a su tierra ancestral, un hombre americano es mordido y posteriormente maldecido por un hombre lobo.", R.drawable.elhombrelobo, "https://www.youtube.com/watch?v=Wff_63MgPPI"))
+        peliculas.add(Pelicula("John Wick (Otro día para matar)", "Un ex-sicario sale de su retiro para perseguir a los gángsters que mataron a su perro y le robaron el coche.", R.drawable.johnwick, "https://www.youtube.com/watch?v=TWRxFTiNTyU"))
+        peliculas.add(Pelicula("Juego de armas", "Basada libremente en la historia real de dos jóvenes, David Packouz y Efraim Diveroli, que consiguieron un contrato de trescientos millones de dólares del Pentágono para armar a los aliados de Estados Unidos en Afganistán.", R.drawable.juegodearmas, "https://www.youtube.com/watch?v=C0eT-Vcovqc"))
+        peliculas.add(Pelicula("Los odiosos ocho", "En pleno invierno de Wyoming, un cazarrecompensas y su prisionero encuentran refugio en una cabaña habitada actualmente por una colección de nefastos personajes.", R.drawable.losodiososocho, "https://www.youtube.com/watch?v=JxVRgBOL8jc"))
+        peliculas.add(Pelicula("Seven", "Dos detectives, un novato y un veterano, dan caza a un asesino en serie que utiliza los siete pecados capitales como motivos.", R.drawable.seven, "https://www.youtube.com/watch?v=znmZoVkCjpI"))
         peliculas.add(Pelicula("El lobo de Wall Street", "Basada en la historia real de Jordan Belfort, desde su ascenso como acaudalado corredor de bolsa hasta su caída en manos del crimen, la corrupción y el gobierno federal.", R.drawable.ellobodewallstreet, ""))
-        peliculas.add(Pelicula("Focus(II)", "En medio del último plan del veterano estafador Nicky, una mujer de su pasado ahora una consumada femme fatale aparece y da al traste con sus planes.", R.drawable.focus, ""))
-        peliculas.add(Pelicula("El corazon de acero", "Un canoso comandante de tanque toma decisiones difíciles mientras él y su tripulación se abren camino a través de Alemania en abril de 1945.", R.drawable.corazonesdehierro, ""))
-        // Agrega más películas según sea necesario
+        peliculas.add(Pelicula("Focus(II)", "En medio del último plan del veterano estafador Nicky, una mujer de su pasado ahora una consumada femme fatale aparece y da al traste con sus planes.", R.drawable.focus, "https://www.youtube.com/watch?v=DEMZSa0esCU"))
+        peliculas.add(Pelicula("El corazon de acero", "Un canoso comandante de tanque toma decisiones difíciles mientras él y su tripulación se abren camino a través de Alemania en abril de 1945.", R.drawable.corazonesdehierro, "https://www.youtube.com/watch?v=yftjMJVN4dE"))
+
 
         return peliculas
     }
@@ -141,9 +138,19 @@ class PeliculasSeries : AppCompatActivity() {
         val series = mutableListOf<Serie>()
 
         // Agregar series a la lista
-        series.add(Serie("Breaking bad", "Un profesor de instituto diagnosticado con cáncer de pulmón empieza a manufacturar y vender metamfetamina para asegurar el futuro de su familia.", R.drawable.breakingbad, "URL Video Serie 1"))
-        series.add(Serie("Hermanos de sangre", "La historia de la Easy Company de la División Aerotransportada 101 del Ejército de los Estados Unidos y su misión en la Segunda Guerra Mundial en Europa, desde la Operación Overlord hasta el Día V-J.", R.drawable.hermanosdesangre, "URL Video Serie 2"))
-        // Agrega más series según sea necesario
+        series.add(Serie("Breaking bad", "Un profesor de instituto diagnosticado con cáncer de pulmón empieza a manufacturar y vender metamfetamina para asegurar el futuro de su familia.", R.drawable.breakingbad, "https://www.youtube.com/watch?v=HhesaQXLuRY"))
+        series.add(Serie("Hermanos de sangre", "La historia de la Easy Company de la División Aerotransportada 101 del Ejército de los Estados Unidos y su misión en la Segunda Guerra Mundial en Europa, desde la Operación Overlord hasta el Día V-J.", R.drawable.hermanosdesangre, "https://www.youtube.com/watch?v=aH06LWZs-Ys"))
+        series.add(Serie("Juego de tronos", "Nueve familias nobles luchan por el control de las tierras de Poniente, mientras un antiguo enemigo regresa tras permanecer inactivo durante milenios.", R.drawable.juegodetronos, ""))
+        series.add(Serie("Chernobyl", "Esta apasionante miniserie narra la impactante historia del peor accidente provocado por el hombre de la historia, siguiendo la tragedia desde el momento de la explosión a primera hora de la mañana hasta el caos y la pérdida de vidas en los días, semanas y meses siguientes.", R.drawable.chernobyl, ""))
+        series.add(Serie("Sherlock", "El peculiar giro del icónico detective de Conan Doyle lo presenta como un \"sociópata de alto funcionamiento\" en el Londres actual. Le ayuda en sus investigaciones: John Watson, veterano de la guerra de Afganistán, que es presentado a Holmes por un conocido común.", R.drawable.sherlock, ""))
+        series.add(Serie("Los Soprano", "El jefe de la mafia de Nueva Jersey Tony Soprano se enfrenta a problemas personales y profesionales en su vida familiar y empresarial que afectan a su estado mental, lo que le lleva a buscar ayuda psiquiátrica profesional.", R.drawable.lossoprano, ""))
+        series.add(Serie("Rick y Morty", "Las fracturadas vidas domésticas de un científico loco nihilista y su ansioso nieto se complican aún más con sus desventuras interdimensionales.", R.drawable.rickymorty, ""))
+        series.add(Serie("Friends", "Sigue la vida personal y profesional de seis amigos de veinte a treinta años que viven en el barrio de Manhattan, en Nueva York.", R.drawable.friends, ""))
+        series.add(Serie("The Office", "Un falso documental sobre un grupo de oficinistas típicos, donde la jornada laboral consiste en choques de egos, comportamientos inapropiados y tedio.", R.drawable.theoffice, ""))
+        series.add(Serie("Ataque a los titane", "Después de que su ciudad natal sea destruida y quede traumatizado, el joven Eren Jaeger jura limpiar la Tierra de los gigantescos Titanes humanoides que han llevado a la humanidad al borde de la extinción.", R.drawable.ataquealostitanes, ""))
+        series.add(Serie("Stranger Things", "Cuando un niño desaparece, un pequeño pueblo descubre un misterio que implica experimentos secretos, terroríficas fuerzas sobrenaturales y una extraña niña.", R.drawable.strangerthings, ""))
+        series.add(Serie("Peaky Blinders", "Una epopeya familiar de gángsters ambientada en la Inglaterra de 1900, centrada en una banda que cose cuchillas de afeitar en los picos de sus gorras, y en su feroz jefe Tommy Shelby.", R.drawable.peakybliders, ""))
+
 
         return series
     }
@@ -246,10 +253,7 @@ data class Pelicula(
     val imagen: Int = 0, // Identificador de recurso de la imagen
     val videoUrl: String = ""
 )
-data class Peli(
-    val nombre: String = "",
 
-)
 
 data class Serie(
     val titulo: String = "",
